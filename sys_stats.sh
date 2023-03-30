56b5b448264be4f7dddf4c8db062b51035bb92f1
Amelius66 <129412723+Amelius66@users.noreply.github.com>
2023-03-30 10:14:41 -0600

    Update sys_stats.sh

--- sys_stats.sh
+++ sys_stats.sh
@@ -1,27 +1,35 @@
 # add shebang here
 # add any contributor names in a comment here
-
+Andrew Mimori
 #************************************#
 #******* OUTPUT SYSTEM STATS ********#
 #************************************#
 
 # Make a directory called System_Stats and change directories to that directory
+
 # Try to make the output for each file look as neat and organized as you can.
 
 # Output the following information to a file called kernel
     # 1. Kernel Name
+    uname -s
     # 2. Kernel Release
+    uname -r
     # 3. Kernel Version
+    uname -v
     # 4. The operating system
+    uname -o
 
 
 # Output the following information to a file called network
     # 1. Do not print any serial numbers or sensitive information related to the system
+    
     # 2. All network interfaces
+    
     
     
 # Output the following information to a file called disk.html
     # 1. All disks
+    
     # 2. The output should include html tags, i.e. <html></html>
 
 
