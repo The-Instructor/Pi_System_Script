93551f9bf307a2706d0824d9345957084aeb82b4
Cupresssoides <129413651+Cupresssoides@users.noreply.github.com>
2023-03-30 10:03:08 -0600

    Update sys_stats.sh
    added numbered tasks and divided up the work

--- sys_stats.sh
+++ sys_stats.sh
@@ -3,13 +3,20 @@
 #************************************#
 #******* OUTPUT SYSTEM STATS ********#
 #************************************#
+# part 1 and 2 will be Liam
+# part 3 and 4 will be Joseph
+# part 5 and 6 will be Tom
+
+# All output will be stored in ~/Documents/System_Stats
 
 # Make a directory called System_Stats and change directories to that directory
 # Try to make the output for each file look as neat and organized as you can.
 
-# Output the following information to a file called kernel
+# 1 Output the following information to a file called kernel
 
-#uname
+
+
+# uname
 
     # 1. Kernel Name
     # 2. Kernel Release
@@ -17,27 +24,27 @@
     # 4. The operating system
 
 
-# Output the following information to a file called network
+# 2 Output the following information to a file called network
     # 1. Do not print any serial numbers or sensitive information related to the system
     # 2. All network interfaces
     
     
-# Output the following information to a file called disk.html
+# 3 Output the following information to a file called disk.html
     # 1. All disks
     # 2. The output should include html tags, i.e. <html></html>
 
 
-# Output the following information to a file called cpu
+# 4 Output the following information to a file called cpu
     # 1. The first five lines of the command lscpu
     # 2. The last 12 lines of the command lscpu
     
 
-# Output the following information to a file called block_dev
+# 5 Output the following information to a file called block_dev
     # 1. Only the name, size, and type of the block devices
     # 2. The output should use ascii characters for any tree formatting
     
     
-# Output the following information to a file called sata
+# 6 Output the following information to a file called sata
     # 1. Any Sata devices connected to the machine along with human readable sizes of the devices
     # 2. More specific information about each device that is connected to the machine
     
