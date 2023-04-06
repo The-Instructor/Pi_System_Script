279f9951899d9095295d6e8ec3f6aa86eee295fa
agiroir <101474170+agiroir@users.noreply.github.com>
2023-04-06 09:40:17 -0600

    Update sys_stats.sh
    adding --ascii to show end of options and applying ascii

--- sys_stats.sh
+++ sys_stats.sh
@@ -38,7 +38,7 @@ mk dir System_Stats | cd System_Stats
     # 1. Only the name, size, and type of the block devices
     # 2. The output should use ascii characters for any tree formatting
     
-    lsblk -o name,size,type ascii > block_dev 
+    lsblk -o name,size,type --ascii > block_dev 
     
 # Output the following information to a file called sata
     # 1. Any Sata devices connected to the machine along with human readable sizes of the devices
